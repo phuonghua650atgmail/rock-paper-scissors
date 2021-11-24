27e0ac3c4d29e1c7856006321dc9becb5fdae2e9
Phuong Hua <phuonghua650@gmail.com>
2021-11-24 11:57:58 -0800

    added randomNumberChoice function to js file

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('console.log: ' + 'test external js file to index.html');
+
+function computerPlay () {
+    let randomNumberChoice = Math.floor(Math.random() * 3) // this ranges from 0 to 2. not 1 to 3. this is why i labeled the cases accordingly
+    switch (randomNumberChoice) {
+        case 0: 
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors'; 
+            break;
+        default: 
+            console.log('looks like you reach the default case. i am not sure how you would get to default.');
+    }
+    alert('alert ' + randomNumberChoice);
+    console.log('console.log ' + randomNumberChoice);
+    return 'return ' + randomNumberChoice;
+}
